8d19f40243fadf04a165c72b58eed518ae8dcba4
Xoán Mallón <60132180+xoanmm@users.noreply.github.com>
2022-03-07 12:20:23 +0100

    fix: add necessary comment and delete innecesary cred configuration (#18)

--- internal/app/git/config.go
+++ internal/app/git/config.go
@@ -2,6 +2,7 @@ package git
 
 import "text/template"
 
+// DefaultGitCommitMessage is the default commit message build with the changes detected in the app
 const DefaultGitCommitMessage = `🚀 automatic update of {{ .AppName }}
 {{ range .KeyChanges -}}
 updates key {{ .Key }} value from '{{ .OldValue }}' to '{{ .NewValue }}'
